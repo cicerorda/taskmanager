c2d257c5c3bbea842bbb518ee8d9f94fa0e47f1b
Dutch <cicero.rda@gmail.com>
2022-03-08 00:13:14 -0300

    Task manager 1.0 finished

--- app/src/main/java/com/example/myapplication/ui/AddTaskActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/AddTaskActivity.kt
@@ -6,8 +6,10 @@ import android.os.PersistableBundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityAddTaskBinding
+import com.example.myapplication.datasource.TaskDataSource
 import com.example.myapplication.extensions.format
 import com.example.myapplication.extensions.text
+import com.example.myapplication.model.Task
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.timepicker.MaterialTimePicker
 import com.google.android.material.timepicker.TimeFormat
@@ -19,6 +21,15 @@ class AddTaskActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAddTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (intent.hasExtra(TASK_ID)) {
+            val taskId = intent.getIntExtra(TASK_ID, 0)
+            TaskDataSource.findById(taskId)?.let {
+                binding.tilTitle.text = it.title
+                binding.tilHour.text = it.hour
+                binding.tilDate.text = it.date
+            }
+        }
 
         insertListeners()
     }
@@ -38,9 +49,35 @@ class AddTaskActivity : AppCompatActivity() {
                 .setTimeFormat(TimeFormat.CLOCK_24H)
                 .build()
             timePicker.addOnPositiveButtonClickListener {
-                binding.tilHour.text = "${timePicker.hour}:${timePicker.minute}"
+
+              val minute = if (timePicker.minute in 0..9) "0${timePicker.minute}" else timePicker.minute
+
+              val hour = if (timePicker.hour in 0..9) "0${timePicker.hour}" else timePicker.hour
+
+                binding.tilHour.text = "$hour:$minute"
             }
             timePicker.show(supportFragmentManager, null)
         }
+        binding.btncancel.setOnClickListener{
+            finish()
+        }
+
+        binding.btnNewTask.setOnClickListener{
+            val task = Task(
+                title = binding.tilTitle.text,
+                date = binding.tilDate.text,
+                hour = binding.tilHour.text,
+                id = intent.getIntExtra(TASK_ID, 0)
+
+            )
+            TaskDataSource.insertTask(task)
+            Log.e("TAG", "insertListeners: " + TaskDataSource.getList() )
+            setResult(Activity.RESULT_OK)
+            finish()
+        }
+    }
+
+    companion object {
+        const val TASK_ID = "task_id"
     }
 }
